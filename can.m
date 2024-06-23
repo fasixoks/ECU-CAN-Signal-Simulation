ebd7d28d41c50d6453391a2493c1b6be745f738a
EVIE 🍒 <126894089+urevie@users.noreply.github.com>
2024-06-23 16:51:38 -0700

    Additional Features and Enhancements: Low-pass Filter, Fourier Transform, RMS Calculation and Additional Plots (Combined Data - Filtered Data - Fourier Transform)

--- can.m
+++ can.m
@@ -8,14 +8,23 @@ amplitude = 50;
 offset = 25;
 data_sine = amplitude * sin(2 * pi * frequency * t);
 data_square = offset + square(2 * pi * frequency * t);
-data = data_sine + data_square;
+data_combined = data_sine + data_square;
+
+fc = 2;
+[b, a] = butter(2, fc/(1/(2*sample_time)));
+data_filtered = filtfilt(b, a, data_combined);
+
+data_fft = fft(data_combined);
+frequencies = (0:num_samples-1)*(1/(num_samples*sample_time));
+
+rms_value = rms(data_combined);
 
 for i = 1:num_samples
     can_message = struct;
     can_message.ID = can_id;
-    can_message.Data = uint8(typecast(int16(data(i)), 'uint8'));
+    can_message.Data = uint8(typecast(int16(data_combined(i)), 'uint8'));
     
-    disp(['Sending CAN message with ID: 0x', dec2hex(can_message.ID), ', Data: ', num2str(data(i))]);
+    disp(['Sending CAN message with ID: 0x', dec2hex(can_message.ID), ', Data: ', num2str(data_combined(i))]);
     
     pause(sample_time);
     
@@ -28,3 +37,24 @@ for i = 1:num_samples
     grid on;
     drawnow;
 end
+
+figure(2);
+subplot(3,1,1);
+plot(t, data_combined);
+title('Combined Data');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+subplot(3,1,2);
+plot(t, data_filtered);
+title('Filtered Data (Low-pass)');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+subplot(3,1,3);
+plot(frequencies, abs(data_fft));
+title('Fourier Transform');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+
+disp(['RMS Value of Combined Data: ', num2str(rms_value)]);
