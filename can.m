bca0ea608ec14c3c40af2ae3de76a612e35478eb
EVIE 🍒 <126894089+urevie@users.noreply.github.com>
2024-06-23 16:47:11 -0700

    Additional features (Combined Waveforms, Parameters Explained and Data Combination)

--- can.m
+++ can.m
@@ -3,7 +3,12 @@ can_id = hex2dec('123');
 sample_time = 0.01;
 
 t = (0:num_samples-1) * sample_time;
-data = 100 * sin(2 * pi * 1 * t);
+frequency = 1;
+amplitude = 50;
+offset = 25;
+data_sine = amplitude * sin(2 * pi * frequency * t);
+data_square = offset + square(2 * pi * frequency * t);
+data = data_sine + data_square;
 
 for i = 1:num_samples
     can_message = struct;
